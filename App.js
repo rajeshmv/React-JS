8f65ad7156d7dec36d296faaabbf5bf036e33f56
Rajesh <rajesh.vuk@gmail.com>
2025-01-19 17:12:41 +0000

    "episode-01"

---
+++ App.js
@@ -0,0 +1,17 @@
+
+const heading = React.createElement("h1",{},"Hello World from React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+rea
+
+root.render(heading);
+
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
+
